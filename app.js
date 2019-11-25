24f117c9cadf075c380185e77fabbf190604379c
DwayneL <dr2@ameritech.net>
2019-11-25 08:25:10 -0600

    added more inquirer functionality to app page. Can log questions and assign roles.

--- app.js
+++ app.js
@@ -25,5 +25,41 @@ inquirer
     ])
 
     .then(answers => {
-        console.log(answers)
+        if (answers.role === "Manager"){
+            inquirer
+                .prompt([
+                    {
+                    type: "input",
+                    message:"What is your office number?",
+                    name: "office"
+                    }
+                ])
+                .then(response => {
+                    console.log("you are a manager and your room number is  " + response.office)
+                })
+        } else if (answers.role === "Engineer"){
+            inquirer
+                .prompt([
+                    {
+                    type: "input",
+                    message: "What is your GitHub Username?",
+                    name: "hubname"
+                    }
+                ])
+                .then(response => {
+                    console.log (answers.name +answers.id + response.hubname)
+                })
+        } else {
+            inquirer
+                .prompt ([
+                    {
+                        type:"input",
+                        message: "What school do you go to?",
+                        name: "schoolname"
+                    }
+                ])
+                .then (response => {
+                    console.log("you are an intern and you currently attend " + response.schoolname)
+                })
+        }
     })
